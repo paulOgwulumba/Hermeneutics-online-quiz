0ebd55e8594b9e8a2d1d7c98d08865eb7ded2c09
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2020-10-12 14:29:17 +0100

    Set up the back-end router to handle the admin log in

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
+//Import express object
 const express = require('express');
+
+//Import routers
+const adminRouter = require('./routers/admin');
+
+//import database object
 const Datastore = require('nedb');
+
+//initialise environment variables
 require('dotenv').config({path: __dirname + '/.env'});
 
+//initialise database objects
 const student_base =  new Datastore({filename: 'student_base.db', autoload: true})
 
+//Initialise express object
 const server = express();
-server.use(express.json({limit: '1mb'}));
+
+//set up server
+server.use(express.json({limit: '2mb'}));
 server.use(express.static('public'));
+server.use('/admin', adminRouter)
 
-console.log(process.env.MY_NAME)
+//console.log(process.env.MY_NAME)
 
-process.env['FIRE_CRACKER'] = "Run my love"
+//process.env['FIRE_CRACKER'] = "Run my love"
 
-console.log(process.env.FIRE_CRACKER)
+//console.log(process.env.FIRE_CRACKER)
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, ()=> {
