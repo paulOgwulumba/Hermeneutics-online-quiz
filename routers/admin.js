b3f6e638d6cf61cef556589eecee7b2479f88636
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2020-10-14 12:49:11 +0100

    delete student info from database in server side
    Clientside trigger of deletion of student info from server-side database was implemented

--- routers/admin.js
+++ routers/admin.js
@@ -12,9 +12,11 @@ const student_db = new Datastore({filename: path.join(__dirname, '..', 'db', 'st
 //validates the log in entries for the admin page
 admin.post('/log-in', (request, response) => {
   if(request.body.username === process.env.ADMIN_USERNAME && request.body.password === process.env.ADMIN_PASSWORD){
+    console.log(`Successful Admin log in confirmed. Time: ${new Date().toLocaleString()}`)
     response.send({status: "OK"})
   }
   else{
+    console.log(`Failed Admin log. Time: ${new Date().toLocaleString()}`)
     response.send({status: "FAILED"})
   }
 })
@@ -27,6 +29,7 @@ admin.post('/student', (request, response) => {
   //Checks for any empty field
   for(let key of objKeys){
     if(obj[key] === ""){
+      console.log(`New student attempted to be added to database but failed due to empty field. Time: ${new Date().toLocaleString()}`)
       response.send({status: "FAILED"})
     }
   }
@@ -34,26 +37,45 @@ admin.post('/student', (request, response) => {
   try{
     student_db.insert(obj, (error, document) => {
       if(error) throw error;
+      console.log(`New student added to database. Time: ${new Date().toLocaleString()}`)
       response.send({status: "OK"})
     })
   }
   catch(e){
+    console.error(e)
     response.send({status: "FAILED"})
   }
 })
 
 //Sends the class list to the clientside
 admin.get('/students', (request, response) => {
-  student_db.find({}, (error, document)=> {
-    if(error) throw error;
-
-    response.send(document)
-  })
+  try{
+    student_db.find({}, (error, document)=> {
+      if(error) throw error;
+      console.log(`Student info sent to clientside. Time: ${new Date().toLocaleString()}`)
+      response.send(document)
+    })
+  }
+  catch(error){
+    console.error(error)
+  }
 })
 
 //deletes a student's info from the database
 admin.delete('/student', (request, response) => {
-  response.send({status: "OK"})
+  try{
+    student_db.remove({_id: request.body._id}, {}, (error, number) => {
+      if(error) throw error
+      console.log(`${number} student info deleted successfully. Time: ${new Date().toLocaleString()}`)
+      response.send({status: "OK"})
+    })
+    
+  }
+  catch(error){
+    console.error(error)
+    response.send({status: "FAILED"})
+  }
+  
 })
 
 module.exports = admin
