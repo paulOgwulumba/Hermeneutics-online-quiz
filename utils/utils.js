fecd959265c572a5250bc9ed8678a6b5f256cebe
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2020-11-08 22:11:25 +0100

    Update utils.js
    set up mongodb locally for testing purposes

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,8 @@
 //URI for mongodb database
-const Mongodb_URI = process.env.URI;
+//const Mongodb_URI = process.env.URI;
+
+//for development purposes only
+const Mongodb_URI = 'mongodb://127.0.0.1:27017'
 
 //mongodb object to connect with
 const MongoClient = require('mongodb').MongoClient;
