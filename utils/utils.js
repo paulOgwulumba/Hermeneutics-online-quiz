377cc6719f0c1a17cfd02a6d642785752eadc662
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2022-01-07 11:40:31 +0000

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@
 //  const Mongodb_URI = process.env.URI;
 
 //  For development purposes only
-const MongodbURI = 'mongodb://127.0.0.1:27017'
+const MongodbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
 
 //  mongodb object to connect with
 const MongoClient = require('mongodb').MongoClient
